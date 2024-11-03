3a9b54e9414eafe4f77bda185f2e3fa75469f92e
JacobHunt135 <jahunt003@gmail.com>
2024-11-03 11:33:08 -0700

    Added some stuff for Profiles and Staff

--- Stage3/src/Profiles.java
+++ Stage3/src/Profiles.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+        
 public class Profiles {
+    
+    private ArrayList<Staff> staff;
+
+    public Profiles() {
+        staff = new ArrayList<Staff>();
+    }
+    
+    public void addStaff(Staff aStaff) {
+        staff.add(aStaff);
+    }
+    
+    public static void menu() {
+        Scanner in = new Scanner(System.in);
+    }
     
 }
--- Stage3/src/Staff.java
+++ Stage3/src/Staff.java
@@ -1,3 +1,12 @@
 public class Staff {
     
+    private int authority;
+    private String name;
+    private String position;
+    private float wage;
+    
+    public Staff(int authority, String name, String position, float wage){
+        
+    }
+    
 }
